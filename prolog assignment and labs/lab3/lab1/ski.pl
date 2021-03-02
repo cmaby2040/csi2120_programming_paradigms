b8de112c01bf99b839af469f922f8debf7dfbddd
cmaby2040 <79934438+cmaby2040@users.noreply.github.com>
2021-03-02 16:29:11 -0500

    Add files via upload

---
+++ prolog assignment and labs/lab3/lab1/ski.pl
@@ -0,0 +1,33 @@
+% Decision to go skiing
+
+% trail conditions
+condition( skyline, green ).
+condition( burma, green ).
+condition( fortune, green ).
+condition( mcclosky, red ).
+
+% no time tuesday thursday CSI2120
+available( monday ).
+available( wednesday ).
+available( friday ).
+
+% high temperatures
+temperature( mild, monday ).
+temperature( mild, tuesday ).
+temperature( verycold, wednesday ).
+temperature( cold, thursday ).
+temperature( warm, friday ).
+
+% available wax
+wax( green, verycold ).
+wax( blueExtra, mild ).
+wax( red, warm ).
+
+route( C ) :- condition( skyline, C ),condition( burma, C ),condition( fortune, C).
+
+goodTempurature(D,mild) :- temperature(mild,D).
+goodTempurature(D,cold) :- temperature(cold,D).
+
+
+goSki(D,W) :- available(D),goodTempurature(D,X), wax(W,X),route(green).
+
